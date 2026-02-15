109ca6669ea8196b09cb817591654da0e546efc7
Pardeep404 <pardeeps0793@gmail.com>
2026-02-15 16:49:58 +0530

    Types In Typescript

---
+++ src/typesInTs.ts
@@ -0,0 +1,10 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5
+
+let channelName = "Pardeep Singh Vlogs"
+
+let chaiFlavour: string = "masala chai"
+chaiFlavour = "Ginger tea"
+
+let chaiPrder: boolean
